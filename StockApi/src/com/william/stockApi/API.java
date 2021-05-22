da94567116c811af1f7ccb78c5c449c4b247f788
Scoutshot <Scoutshot@DESKTOP-T2K1LL9>
2021-05-22 18:21:55 +0100

    Creation of the Java API

---
+++ StockApi/src/com/william/stockApi/API.java
@@ -0,0 +1,16 @@
+package com.william.stockApi;
+
+public class API {
+	
+	public static String getString() {
+		String fish = "fish";
+		return fish;
+		
+	}
+	
+	public static String getString2() {
+		String fish = "orange";
+		return fish;
+	}
+
+}
